b63167e942cc2441e0a37819eff3a519a0c30ea7
brianteh <brianteh007@gmail.com>
2024-09-28 09:57:33 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import json
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+#GET example
+@app.route('/hello', methods=['GET'])
+def get_employees():
+ return jsonify(["Hello"])
+if __name__ == '__main__':
+   app.run(port=5000)
